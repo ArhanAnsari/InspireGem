14fe87feacf96bc632876cdc61df8e41b0231a83
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-09 20:43:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 // app/layout.tsx
-import '../styles/globals.css';
+import './styles/globals.css';
 import React from 'react';
 import Header from '../components/Header';
 import { ToastContainer } from 'react-toastify';
