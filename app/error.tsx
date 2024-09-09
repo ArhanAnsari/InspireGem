73db978473eb27053e8f0015cf91a6c1046a1073
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-09 20:45:19 +0530

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -1,10 +1,13 @@
 // app/error.tsx
+"use client";
+
 import React from 'react';
 
 const ErrorPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Something went wrong!</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold mb-4">Something went wrong</h1>
+      <p>Please try again later or contact support.</p>
     </div>
   );
 };
