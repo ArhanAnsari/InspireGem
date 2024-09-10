a88cb29f26c322be741ef6f332f46af38ac089c6
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-10 11:08:18 +0530

    Update page.tsx

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,11 +1,11 @@
 // app/auth/error/page.tsx
 "use client";
 
-export default function ErrorPage() {
+export default function AuthErrorPage() {
   return (
     <div>
       <h1>Authentication Error</h1>
-      <p>There was an error with your authentication request.</p>
+      <p>There was an issue with your authentication request.</p>
     </div>
   );
 }
