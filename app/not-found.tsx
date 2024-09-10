5e8f50a9c2c3b6c27db445f969bf0f82d8289882
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-10 11:05:09 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,11 @@
 // app/not-found.tsx
-'use client';
+"use client";
 
-import React from 'react';
-
-const NotFoundPage = () => {
+export default function NotFound() {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold">404 - Not Found</h1>
+    <div>
+      <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </div>
   );
-};
-
-export default NotFoundPage;
+}
