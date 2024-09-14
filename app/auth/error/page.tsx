ab74ba9d704655538335e71097177c7f1bfd722b
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-14 12:50:18 +0530

    Update page.tsx

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,10 +1,13 @@
 // app/auth/error/page.tsx
+"use client";
+import Link from "next/link";
 
-export default function AuthErrorPage() {
+export default function Error() {
   return (
     <div>
-      <h1>Authentication Error</h1>
-      <p>There was an issue with your authentication request.</p>
+      <h1>Error</h1>
+      <p>Something went wrong during the authentication process.</p>
+      <Link href="/auth/signin">Go back to Sign In</Link>
     </div>
   );
 }
