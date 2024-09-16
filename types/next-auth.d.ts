a3abf74a50b79fc0df96c02010e6f7e64b17f549
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-16 12:44:22 +0530

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,17 @@
+// types/next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+  interface JWT {
+    id: string;
+  }
+}
