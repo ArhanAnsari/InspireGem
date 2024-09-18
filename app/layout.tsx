e960722fa15148dc39d961bd23ababb23ca8865e
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-18 16:03:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,23 +5,27 @@ import Header from "../components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "@/components/Layout";
+import { SessionProvider } from "next-auth/react"; // Import SessionProvider
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <body className="bg-gray-50">
-        <Layout>
-          <Header />
-          <main className="min-h-screen flex flex-col items-center justify-center py-12">
-            {children}
-          </main>
-          {/* Toast notifications container */}
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar
-          />
-        </Layout>
+        {/* Wrap with SessionProvider */}
+        <SessionProvider>
+          <Layout>
+            <Header />
+            <main className="min-h-screen flex flex-col items-center justify-center py-12">
+              {children}
+            </main>
+            {/* Toast notifications container */}
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar
+            />
+          </Layout>
+        </SessionProvider>
       </body>
     </html>
   );
