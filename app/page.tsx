8ba6fdd3f0c5c4e17cdeefbfb368163c040d6433
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-18 16:48:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,18 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center text-center">
-      <h1 className="text-4xl font-bold mb-4">
-        Welcome to the Innovify
+    <div className="flex flex-col items-center justify-center text-center min-h-screen bg-gradient-to-b from-blue-50 to-blue-100">
+      <h1 className="text-5xl font-extrabold text-gray-900 mb-6">
+        Welcome to InspireGem
       </h1>
-      <p className="text-gray-600 mb-8">
-        Leverage the power of AI with Google Gemini to create amazing content
-        effortlessly.
+      <p className="text-lg text-gray-700 max-w-lg mb-10">
+        Leverage the power of AI with Google Gemini to create amazing content effortlessly.
       </p>
       <Link href="/plans">
-        <p className="text-white bg-blue-500 hover:bg-blue-600 font-bold py-2 px-4 rounded">
+        <p className="text-white bg-blue-600 hover:bg-blue-700 transition-all font-bold py-3 px-6 rounded-full shadow-lg">
           View Plans
         </p>
       </Link>
     </div>
   );
 }
-
-//export default HomePage;
