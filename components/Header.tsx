076ddba1d5af554c926c8c277ffc804947afce2d
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-18 16:13:52 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-white text-xl">MyApp</div>
+        <div className="text-white text-xl">Innovify</div>
         <ul className="flex space-x-4">
           <li>
             <Link href="/" className="text-white">Home</Link>
