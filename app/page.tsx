47dd000a1780daeaae0df8a13138e9f5ba4b6608
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-25 10:10:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 // Import the StarIcon from Heroicons
-import { StarIcon } from "@heroicons/react";
+import { StarIcon } from "@heroicons/react/24/solid";
 
 export default function HomePage() {
   return (
