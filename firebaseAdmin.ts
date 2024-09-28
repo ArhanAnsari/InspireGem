5f52a212fc88b7f4e6f69dd7925a05b8ec3117ae
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-28 19:25:39 +0530

    Update firebaseAdmin.ts

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,16 +1,20 @@
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
+
 let app: App;
 
+// Initialize the Firebase Admin SDK if no other apps are initialized
 if (getApps().length === 0) {
-  const serviceKey = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT_KEY! as string
-  );
+  // Parse the service account key from environment variable
+  const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
+
+  // Initialize the app with the service account key
   app = initializeApp({
     credential: cert(serviceKey),
   });
 } else {
+  // Use the existing app instance if already initialized
   app = getApp();
 }
 
@@ -18,4 +22,5 @@ if (getApps().length === 0) {
 const adminDb = getFirestore(app);
 const adminStorage = getStorage(app);
 
+// Export the app, Firestore, and Storage instances
 export { app as adminApp, adminDb, adminStorage };
