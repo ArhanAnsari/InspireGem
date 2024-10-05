aed6d45e10264bfcfd68c0206bea904c2f17a2d6
Arhan Ansari <arhanansari2009@gmail.com>
2024-10-05 23:01:50 +0530

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,5 @@
+// lib/stripe.ts
+
 import Stripe from "stripe";
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
@@ -6,6 +8,8 @@ if (!stripeSecretKey) {
   throw new Error("STRIPE_SECRET_KEY is not set");
 }
 
-const stripe = new Stripe(stripeSecretKey);
+const stripe = new Stripe(stripeSecretKey, {
+  apiVersion: "2024-06-20",
+});
 
 export default stripe;
