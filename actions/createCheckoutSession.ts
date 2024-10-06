a04b3b4ca33a06d7ca765aaa0ab4bcfae377b5ea
Arhan Ansari <arhanansari2009@gmail.com>
2024-10-06 12:20:39 +0530

    Update createCheckoutSession.ts

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -6,19 +6,21 @@ import getBaseUrl from "@/lib/getBaseUrl";
 import stripe from "@/lib/stripe";
 import { auth } from "@clerk/nextjs/server";
 
-export async function createCheckoutSession(userDetails: UserDetails) {
+export async function createCheckoutSession(userDetails: UserDetails, plan: string) {
   const { userId } = await auth();
 
   if (!userId) {
     throw new Error("User not found");
   }
 
-  // Check if the user already has a Stripe customer ID in Firestore
-  const userDoc = await adminDb.collection("users").doc(userId).get();
-  let stripeCustomerId = userDoc.data()?.stripeCustomerId;
+  // first check if the user already has a stripeCustomerId
+  let stripeCustomerId;
 
-  // If no Stripe customer ID, create a new Stripe customer
+  const user = await adminDb.collection("users").doc(userId).get();
+  stripeCustomerId = user.data()?.stripeCustomerId;
+
   if (!stripeCustomerId) {
+    // create a new stripe customer
     const customer = await stripe.customers.create({
       email: userDetails.email,
       name: userDetails.name,
@@ -27,28 +29,39 @@ export async function createCheckoutSession(userDetails: UserDetails) {
       },
     });
 
-    // Save the customer ID in Firestore for future reference
-    await adminDb.collection("users").doc(userId).update({
+    await adminDb.collection("users").doc(userId).set({
       stripeCustomerId: customer.id,
     });
 
     stripeCustomerId = customer.id;
   }
 
-  // Create a new checkout session for subscription
+  // Set priceId based on the selected plan
+  let priceId;
+  if (plan === "pro") {
+    priceId = "price_1Q0L3aSCr1Ne8DGFAI9n4GbW"; // Pro plan
+  } else if (plan === "enterprise") {
+    priceId = "price_1Q0L3aSCr1Ne8DGF3yD1iMnd"; // Enterprise plan
+  } else {
+    return NextResponse.json(
+      { error: "Invalid plan selected" },
+      { status: 400 }
+    );
+  }
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
       {
-        price: "price_1PprpjSCr1Ne8DGFURTf2bSL", // Adjust the price ID based on InspireGem's pricing
+        price: priceId, // Dynamic price ID
         quantity: 1,
       },
     ],
     mode: "subscription",
     customer: stripeCustomerId,
-    success_url: `${getBaseUrl()}/dashboard?upgrade=true`, // Redirect user back to dashboard after successful upgrade
-    cancel_url: `${getBaseUrl()}/dashboard/upgrade`, // Cancel URL for failed payments
+    success_url: `${getBaseUrl()}/dashboard?upgrade=true`,
+    cancel_url: `${getBaseUrl()}/dashboard/upgrade`,
   });
 
-  return session.id; // Return the session ID to be used in the frontend
+  return session.id;
 }
