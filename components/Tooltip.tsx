d6359b008044449bbf2bc98c58c451ff58e92f45
Arhan Ansari <arhanansari2009@gmail.com>
2024-10-08 01:36:01 +0530

    Update Tooltip.tsx

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Tooltip as ReactTooltip } from "react-tooltip"; // Use named import
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 const Tooltip: React.FC<{ id: string; text: string; children: React.ReactNode }> = ({ id, text, children }) => (
   <>
     <span className="relative cursor-pointer underline" data-tooltip-id={id} data-tooltip-content={text}>
       {children}
     </span>
-    <ReactTooltip id={id} effect="solid" />
+    <ReactTooltip id={id} />
   </>
 );
 
