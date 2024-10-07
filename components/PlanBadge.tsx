2f9dd96e7a854b1015ad6980d0a4bffe39357840
Arhan Ansari <arhanansari2009@gmail.com>
2024-10-08 00:56:23 +0530

    Create PlanBadge.tsx

---
+++ components/PlanBadge.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type PlanBadgeProps = {
+  plan: string;
+};
+
+const PlanBadge: React.FC<PlanBadgeProps> = ({ plan }) => {
+  let badgeText = "";
+  let badgeColor = "";
+
+  switch (plan) {
+    case "free":
+      badgeText = "Starter";
+      badgeColor = "bg-gray-500";
+      break;
+    case "pro":
+      badgeText = "Pro User";
+      badgeColor = "bg-green-500";
+      break;
+    case "enterprise":
+      badgeText = "AI Enthusiast";
+      badgeColor = "bg-red-500";
+      break;
+    default:
+      badgeText = "Free";
+      badgeColor = "bg-gray-500";
+  }
+
+  return (
+    <div className={`inline-block px-4 py-2 rounded-full text-white ${badgeColor}`}>
+      {badgeText}
+    </div>
+  );
+};
+
+export default PlanBadge;
