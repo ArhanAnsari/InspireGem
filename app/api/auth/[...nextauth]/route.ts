f584b0213eb64eb1fade5a0da1182b00867eed59
Arhan Ansari <arhanansari2009@gmail.com>
2024-10-18 22:59:35 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+// app/api/auth/[...nextauth]/route.ts
 import NextAuth, { NextAuthOptions, User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { adminDb } from "@/firebaseAdmin"; // Import Firestore Admin instance
+import { adminDb } from "@/firebaseAdmin";
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -10,47 +11,45 @@ const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
   ],
-  adapter: FirestoreAdapter(adminDb), // Use the Firebase Admin Firestore instance
+  adapter: FirestoreAdapter(adminDb),
   callbacks: {
     async signIn({ user }: { user: User }) {
       const userEmail = user.email;
 
       if (!userEmail) {
         console.error("No email found for user");
-        return false; // Block sign-in if no email
+        return false;
       }
 
-      // Reference to the user's document in Firestore
       const userDocRef = adminDb.collection("users").doc(userEmail);
       const userDoc = await userDocRef.get();
 
       if (userDoc.exists) {
-        // User already exists, fetch plan and request count
         const userData = userDoc.data();
         console.log("User Plan:", userData?.plan);
         console.log("Request Count:", userData?.requestCount);
       } else {
-        // New user, create a default entry in Firestore
         const newUser = {
-          plan: "free", // Default plan
-          requestCount: 0, // Default request count
+          plan: "free",
+          requestCount: 0,
         };
         await userDocRef.set(newUser);
         console.log("New user created with Free plan and 0 request count.");
       }
 
-      return true; // Allow sign-in
+      return true;
     },
     async session({ session }) {
-      // Pass session information
       return session;
     },
   },
 };
 
-// Create the NextAuth handler
 const handler = NextAuth(authOptions);
 
-// Export the handler for POST and GET requests
 export { handler as GET, handler as POST };
