0bcc077b9cbdfcba53b423f492cd9318f993c8e1
Arhan Ansari <arhanansari2009@gmail.com>
2024-10-26 22:43:24 +0530

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -204,7 +204,7 @@ const ProfilePage = () => {
         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleSignOut}
       >
-        Sign out
+        Sign Out
       </button>
     </div>
   );
