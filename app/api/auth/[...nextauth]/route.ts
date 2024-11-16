267d2081a040c383d1e57188f375c0892f25017c
Arhan Ansari <arhanansari2009@gmail.com>
2024-11-16 17:59:55 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,8 @@
 //app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+import { authOptions } from "@/auth";
+import NextAuth from "next-auth";
+
+const handler = NextAuth(authOptions);
+
+export const GET = handler;
+export const POST = handler;
